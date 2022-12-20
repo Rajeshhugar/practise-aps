c70ea1bd7f108f49a1ceaf7d41c0ff7521fb9baf
rajeshXT <hugarrajesh@gmail.com>
2022-12-20 09:53:29 +0530

    packages created

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import pymongo 
-
-# Provide the mongodb  localhost url t oconnect python to mongodb
-client =pymongo.MongoClient('mongodb://localhost:27017')
-
-# Database Name
-database = client[]
-
-# Collection Name 
-collection = database['Products']
-
-
-# Sample Data 
-d = {}
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys,os
+def test_logger_and_exception():
+    try:
+        logging.info("Starting the test_logger_and_exception")
+        
+        result = 3/0
+        print(result)
+        
+        logging.info('Stopping the test_logger_and_exception')
+    except Exception as e:
+        raise SensorException(e,sys)
+        
+if __name__== "__main__":
+    try:
+        test_logger_and_exception()
+        
+    except Exception as e:
+        print(e)
+        
--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ from typing import List
 REQUIREMENTS_FILE_NAME = 'requirements.txt'
 HYPEN_E_DOT = '-e .'
 
-def get_requirements()->List[str]:...
-   with open(REQUIREMENTS_FILE_NAME) as requireemnt_file:
-       requirement_list =requireemnt_file.readlines()
-       requirement_list =[requirement_name.replace ("\n","") for requirement_name in 
-        requirement_list]
-    if HYPHEN_E_DOT in requirement_list:
-        requirement_list.remove(HYPEN_E_DOT)
+def get_requirements()->List[str]:
+    with open(REQUIREMENTS_FILE_NAME) as requirement_file:
+       requirement_list =requirement_file.readlines()
+       requirement_list =[requirement_name.replace ("\n","") for requirement_name in requirement_list]
+    if HYPEN_E_DOT in requirement_list:
+       requirement_list.remove(HYPEN_E_DOT)
     return requirement_list
 
 
